43211029ae5ba2649d1e76b17d9d314e92d1ee6d
Alyssa <alyssapadayhag@gmail.com>
2022-06-23 01:35:47 -0400

    add CreateDeck

---
+++ src/Layout/CreateDeck.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function CreateDeck() {
+    return (  
+        <div>
+            
+            <button type="button" class="btn btn-secondary">
+                <img src="/open-iconic/svg/plus.svg" />
+                Create Deck
+            </button>
+        </div>
+    );
+}
+
+export default CreateDeck;
--- src/Layout/index.js
+++ src/Layout/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
 
+
+
+import { BrowserRouter as Link, Route, Switch } from 'react-router-dom';
+
 function Layout() {
   return (
     <div>
       <Header />
       <div className="container">
         {/* TODO: Implement the screen starting here */}
+        <CreateDeck />
         <NotFound />
       </div>
     </div>
